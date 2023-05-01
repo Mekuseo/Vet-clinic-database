ba4c61a7ec4788371535536f7ef7027188d605e0
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-05-01 17:01:07 +0100

    edited table creation

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY,
     name VARCHAR(255),
     date_of_birth DATE,
     escape_attempts INTEGER,
